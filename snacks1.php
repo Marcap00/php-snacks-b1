f1d1722faeebc81096e918bb9b5fe60e46c126d1
Marcap00 <cappellatomarco@yahoo.com>
2024-10-04 11:53:19 +0200

    first commit

---
+++ snacks1.php
@@ -0,0 +1,72 @@
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 1</title>
+</head>
+
+<body>
+    <ul>
+        <?php foreach ($posts as $date => $post) { ?>
+        <li>
+            <h2><?php echo $date; ?></h2>
+            <ul>
+                <?php foreach ($post as $post_value) { ?>
+                <li>
+                    <h3><?php echo $post_value['title']; ?></h3>
+                    <p><?php echo $post_value['author']; ?></p>
+                    <p><?php echo $post_value['text']; ?></p>
+                </li>
+                <?php } ?>
+            </ul>
+        </li>
+        <?php } ?>
+    </ul>
+</body>
+
+</html>
---
+++ snacks2.php
@@ -0,0 +1,33 @@
+<!-- Con un form passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare "Accesso riuscito", altrimenti "Accesso negato" -->
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snacks 2</title>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css'
+        integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg=='
+        crossorigin='anonymous' />
+</head>
+
+<body>
+    <form class="w-50" action="snacks2.php" method="GET">
+        <div class="mb-3">
+            <input class="form-control" type="text" name="name" id="name" placeholder="Name">
+        </div>
+        <div class="mb-3">
+            <input class="form-control" type="email" name="mail" placeholder="Mail">
+        </div>
+        <div class="mb-3">
+            <input class="form-control" type="text" name="age" placeholder="Age">
+        </div>
+        <button class="btn btn-primary" type="submit">Submit</button>
+    </form>
+
+</body>
+
+</html>
